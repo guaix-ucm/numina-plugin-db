36c1fe111ebf249da406d15de29b22a12853be45
Sergio Pascual <sergiopr@fis.ucm.es>
2018-03-20 23:48:30 +0100

    Store db keys in common module

---
+++ numinadb/dbkeys.py
@@ -0,0 +1,24 @@
+
+DB_PRODUCT_KEYS = [
+    'instrument',
+    'observation_date',
+    'uuid',
+    'quality_control'
+]
+
+
+DB_FRAME_KEYS = [
+    'instrument',
+    'object',
+    'observation_date',
+    'uuid',
+    'type',
+    'mode',
+    'exptime',
+    'darktime',
+    'insconf',
+    'blckuuid',
+    'quality_control',
+    'vph',
+    'insmode'
+]
